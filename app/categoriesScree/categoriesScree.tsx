4323f8121eea9cab3df8e2a637220e0e8d4077e7
manishp <manishprocks013@gmail.com>
2025-03-04 20:21:19 +0530

    wrapped in global container
    wrapped in global container

--- app/categoriesScree/categoriesScree.tsx
+++ app/categoriesScree/categoriesScree.tsx
@@ -1,85 +1,97 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, SafeAreaView, FlatList, Dimensions } from 'react-native';
-import Header from '@/components/Header';
-import colors from '../config/colors';
-import CategoryItem from '../components/CategoryItem';
-import { redirectToPage } from '@/utilities/redirectionHelper';
-import containers from '@/containers';
-import Footer from '@/components/Footer';
-import { router, useLocalSearchParams } from 'expo-router';
-import styles from './categoriesScreeStyles';
-import { categoryService,Category } from '@/services/categoryService';
-
-
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  FlatList,
+  Dimensions,
+  ScrollView,
+} from "react-native";
+import Header from "@/components/Header";
+import colors from "../config/colors";
+import CategoryItem from "../components/CategoryItem";
+import { redirectToPage } from "@/utilities/redirectionHelper";
+import containers from "@/containers";
+import Footer from "@/components/Footer";
+import { router, useLocalSearchParams } from "expo-router";
+import styles from "./categoriesScreeStyles";
+import { categoryService, Category } from "@/services/categoryService";
+import { globalStyles } from "@/assets/styles/globalStyles";
 
 const categoriesScreen = () => {
+  const [subCategories, setsubCategories] = useState<Category[]>([]);
+  const [category, setCategory] = useState<Category>();
+  const { categoryId } = useLocalSearchParams();
 
-    const [subCategories, setsubCategories] = useState<Category[]>([]);
-    const [category,setCategory] = useState<Category>();
-    const {categoryId}= useLocalSearchParams();
-
-    useEffect(()=>{
-      console.log("categoryId",categoryId)
-      const fetchCategory = async () =>{
+  useEffect(() => {
+    console.log("categoryId", categoryId);
+    const fetchCategory = async () => {
       const data = await categoryService.getCategoryById(Number(categoryId));
       console.log(data);
       setCategory(data);
+    };
+    fetchCategory();
+  }, []);
+
+  useEffect(() => {
+    const fetchSubCategories = async () => {
+      try {
+        const data = await categoryService.getAllSubCategories(
+          Number(categoryId)
+        );
+        console.log(data);
+        setsubCategories(data);
+      } catch (error) {
+        console.error("Error fetching categories:", error);
       }
-      fetchCategory();
+    };
 
-    },[])
-  
-    
-  
-    useEffect(() => {
-      const fetchSubCategories = async () => {
-        try {
-          const data = await categoryService.getAllSubCategories(Number(categoryId));
-          console.log(data);
-          setsubCategories(data);
-        } catch (error) {
-          console.error('Error fetching categories:', error);
-        } 
-      };
-  
-      fetchSubCategories();
-    }, []);
+    fetchSubCategories();
+  }, []);
 
-    const renderItem = ({ item, index }: { item: any; index: number }) => {
-      const isEven = index % 2 === 0;
-      return (
-        <View
-          style={[
-            styles.categoryItem,
-            isEven ? styles.leftItem : styles.rightItem,
-          ]}
-        >
-          <CategoryItem
+  const renderItem = ({ item, index }: { item: any; index: number }) => {
+    const isEven = index % 2 === 0;
+    return (
+      <View
+        style={[
+          styles.categoryItem,
+          isEven ? styles.leftItem : styles.rightItem,
+        ]}
+      >
+        <CategoryItem
           title={item.name}
-            image={item.images?.[0] || ""}
-            onPress={() =>redirectToPage(containers.searchResultsScreenScreen, { fromSearch: true, category: item.name,categoryId: item.id })}
+          image={item.images?.[0] || ""}
+          onPress={() =>
+            redirectToPage(containers.searchResultsScreenScreen, {
+              fromSearch: true,
+              category: item.name,
+              categoryId: item.id,
+            })
+          }
+        />
+      </View>
+    );
+  };
+  return (
+    <View style={[globalStyles.container]}>
+      <Header headerText={category?.name} />
+      <ScrollView>
+        <View style={[{ backgroundColor: colors.white, flex: 1 }]}>
+          <FlatList
+            data={subCategories}
+            keyExtractor={(item) => item.id.toString()}
+            renderItem={renderItem}
+            numColumns={2}
+            columnWrapperStyle={styles.row}
+            contentContainerStyle={styles.listContainer}
+            showsVerticalScrollIndicator={false}
           />
         </View>
-      );
-    };
-  return (
-    <SafeAreaView style={[styles.container, { backgroundColor: colors.white }]}>
-    <Header headerText={ category?.name} />
-    <View style={[ { backgroundColor: colors.white }]}>
-      <FlatList
-        data={subCategories}
-        keyExtractor={(item) => item.id.toString()}
-        renderItem={renderItem}
-        numColumns={2}
-        columnWrapperStyle={styles.row}
-        contentContainerStyle={styles.listContainer}
-        showsVerticalScrollIndicator={false}
-      />
+      </ScrollView>
+      <Footer navigation={router} activeTab="home" />
     </View>
-    <Footer navigation={router} activeTab="home" />
-  </SafeAreaView>
   );
 };
-
 
 export default categoriesScreen;
