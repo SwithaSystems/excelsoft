808327dd805e27c3ca5fd6a263e95eda14aa3acc
Sudha Kondi <sudha.kondi@swithasystems.com>
2025-06-03 03:22:48 +0530

    logout redirected

--- services/auth.service.ts
+++ services/auth.service.ts
@@ -2,6 +2,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { router } from "expo-router";
 import containers from "@/containers";
 import { jsonAxios } from "./axiosConfig";
+import { redirectToPage } from "@/utilities/redirectionHelper";
 
 export const authService = {
   async login(phone: string, password: string) {
@@ -40,7 +41,8 @@ export const authService = {
     try {
       await AsyncStorage.removeItem("token");
       await AsyncStorage.removeItem("user");
-      router.replace("/(auth)/signIn" as any);
+      // router.replace("/auth/signIn" as any);
+      redirectToPage(containers.signInScreen);
     } catch (error) {
       throw error;
     }
