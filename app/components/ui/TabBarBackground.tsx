7d086bb3ed7260b6f92a44e576200b91a2a87d62
LakshmipriyaB012 <lakshmipriya.bolagani@swithasystems.com>
2025-07-25 15:09:29 +0530

    fallback file errors

--- app/components/ui/TabBarBackground.tsx
+++ app/components/ui/TabBarBackground.tsx
@@ -1,6 +1,19 @@
 // This is a shim for web and Android where the tab bar is generally opaque.
-export default undefined;
+// export default undefined;
 
+// export function useBottomTabOverflow() {
+//   return 0;
+// }
+import { Platform } from "react-native";
+ 
 export function useBottomTabOverflow() {
+  if (Platform.OS === "ios") {
+    const { useBottomTabBarHeight } = require("@react-navigation/bottom-tabs");
+    const { useSafeAreaInsets } = require("react-native-safe-area-context");
+ 
+    const tabHeight = useBottomTabBarHeight();
+    const { bottom } = useSafeAreaInsets();
+    return tabHeight - bottom;
+  }
   return 0;
 }
