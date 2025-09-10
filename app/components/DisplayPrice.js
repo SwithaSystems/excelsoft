6f20f2c334f0cbb53540870357c0d0585e51fc49
Sudha Kondi <sudha.kondi@swithasystems.com>
2025-09-10 18:00:36 +0530

    Display issue fixed

--- app/components/DisplayPrice.js
+++ app/components/DisplayPrice.js
@@ -4,25 +4,27 @@ import { StyleSheet, Text, View } from "react-native";
 import CurrencySymbol from "@/constants/CurrencySymbol";
 
 function DisplayPrice(props) {
-  const { discount, netPrice } = props;
+  const { discount = 0, netPrice = 0 } = props;
+
+  const finalPrice = (Number(netPrice) - Number(discount)).toFixed(2);
+
   return (
-    <>
-      <View style={props?.priceContainerStyle || styles.priceContainerStyle}>
-        <Text style={props?.priceStyle || styles.netPrice}>
+    <View style={props?.priceContainerStyle || styles.priceContainerStyle}>
+      <Text style={props?.priceStyle || styles.netPrice}>
+        {CurrencySymbol}
+        {finalPrice}
+      </Text>
+
+      {discount > 0 && Number(netPrice) > Number(discount) && (
+        <Text style={props?.strikeOffPriceStyle || styles.discount}>
           {CurrencySymbol}
-          {netPrice?.toFixed(2) - discount?.toFixed(2)}
+          {Number(netPrice).toFixed(2)}
         </Text>
-        {props?.discount &&
-          props?.netPrice.toFixed(2) - props?.discount.toFixed(2) != 0 && (
-            <Text style={props?.strikeOffPriceStyle || styles.discount}>
-              {CurrencySymbol}
-              {netPrice.toFixed(2)}
-            </Text>
-          )}
-      </View>
-    </>
+      )}
+    </View>
   );
 }
+
 const styles = StyleSheet.create({
   priceContainerStyle: {
     flexDirection: "row",
@@ -40,4 +42,5 @@ const styles = StyleSheet.create({
     marginLeft: 6,
   },
 });
+
 export default DisplayPrice;
